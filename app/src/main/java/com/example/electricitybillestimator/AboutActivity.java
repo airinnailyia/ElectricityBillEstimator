6c6223bd7df5ed2237d38271c84e4137fbd1e576
Airin <2024124255@student.uitm.edu.my>
2026-06-01 13:39:07 +0800

    message

--- app/src/main/java/com/example/electricitybillestimator/AboutActivity.java
+++ app/src/main/java/com/example/electricitybillestimator/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.example.electricitybillestimator;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +10,14 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView githubLink = findViewById(R.id.githubLink);
+
+        githubLink.setMovementMethod(
+                LinkMovementMethod.getInstance()
+        );
     }
 }
